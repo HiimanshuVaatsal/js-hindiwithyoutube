0837ee99de1f83fabdee2da496906524146b7032
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2023-08-22 09:13:19 +0000

    conversion of dataType

---
+++ 01_basics/conversion.js
@@ -0,0 +1,28 @@
+let score ="hiimanshu"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=> NaN
+//true=>1,false=>0
+
+
+let isLoggedIn = 'Hiimansuj'
+// let booleanIsLoggedIn = BooleanI(isLoggedIn);
+// console.log(booleanIsLoogedIn);
+
+// 1 => true, 0 => false;
+// "" => false;
+// "hii" => true;
+
+let someNumber =33
+
+let stringNumber = String (someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
