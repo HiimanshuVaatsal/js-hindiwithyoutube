bfcdcf51c74e8f8321946657647e477d76f23583
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2023-08-31 12:15:53 +0000

    StringClass

---
+++ String.js
@@ -0,0 +1,37 @@
+const name='Himanshuk'
+const repoCount=40
+//console.log(name + repoCount + "value"); This is old 
+
+// console.log(`Hello my name is  ${name} and my repo count is ${repoCount}`);
+
+const gameName =new String('Himanshuk-hv-lion')
+
+// console.log(gameName[0]);
+// console.log(gameName. __proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2)); here we check that any index which value.
+// console.log(gameName.indexOf('t'));here we check that any value which index.
+
+
+// const newString=gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString=gameName.slice(-6,4);
+// console.log(anotherString);
+
+
+const newStringOne= "        Hiimanshu    ";
+// console.log(newStringOne);
+// console.log(newStringOne.length);
+// console.log(newStringOne.trim().length);
+
+// const url='https://hiimanshuk.com/hiimanshuk%20vatsal'
+// console.log(url.replace('%20','_'));
+// console.log(url.includes('hiimanshuk'))
+
+
+// console.log(gameName.split('-'));
+
