425732e2662415c45a7f3213b56afc7ed3202a39
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2023-08-31 12:38:57 +0000

    NumberandMath

---
+++ NumberMath.js
@@ -0,0 +1,40 @@
+// const score=400;
+// console.log(score); here by defaul score is number type;
+
+// const balance=new Number(100)
+// console.log(balance); here we are declare stricktly this is number type;
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); here is anw like 100.00 here after decimal you want to show only two value.
+
+// const otherNumber=124.8944;
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//+++++++++++++++++++++ Maths ++++++++++++++++
+
+
+console.log(Math);
+
+// console.log(Math.abs(-4)); here we find absolute value like -4 convert to 4  but 4 is 4
+
+// console.log(Math.round(4.6)); 5
+// console.log(Math.ceil(4.2)); 5
+// console.log(Math.floor(4.5));4
+// console.log(Math.min(4,3,2,6,7));
+// console.log(Math.max(4,3,6,8,4,3));
+
+
+// console.log(Math.random()); here in rendom function value between 0 to 1 only
+
+// console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random())*(max-min+1))
+
