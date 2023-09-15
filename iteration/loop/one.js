bae4dbf6f769bb70faf8d0e906a8948deeb0cf99
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2023-09-15 04:39:23 +0000

    loop

---
+++ iteration/loop/one.js
@@ -0,0 +1,19 @@
+// for loop
+
+// for(let i = 0;i <= 5; i++){
+//     const element = i;
+//     if (element == 5){
+//       console.log('5 is best number');
+//     }
+//     console.log(element);
+// }
+
+
+//nested loop 
+
+// for(let i = 0; i < 10; i++){
+//     console.log('outerlopp');  first time 
+//     for(let j = 0; j < 2; j++){
+//         console.log('innerloop'); two time 
+//     }
+// }
