1f84e6abcfe168b7fb5139a4183068e74d5732fb
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2023-09-22 08:27:31 +0000

    Promises

--- AdvanceJS/Promises.js
+++ AdvanceJS/Promises.js
@@ -28,11 +28,64 @@ new Promise(function(resolve,reject){
 
 const Promisethird = new Promise(function(resolve, reject){
     setTimeout(()=>{
-      console.log("create the promises");
-      resolve();
+    //   console.log("create the promises");
+      resolve({username:"Himanshuk", age:24, course:'MCA'});
     },2000)
 })
 
-Promisethird.then(()=>{
-    console.log("caputer the promises");
+Promisethird.then((user)=>{
+    // console.log(user.username);
+    // console.log(user.age);
+    // console.log(user.course);
 })
+
+
+
+// 4th type promises how to avoid promises hall
+
+// const promiseFour = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=false;
+//         if(!error){
+//             resolve({usrename:'HV',password:'124'})
+//         }else{
+//             reject("Error:Something went wrong");
+//         }
+//     },1000)
+// })
+
+// promiseFour.then((user)=>{
+//    console.log(user);
+//    return user.username
+// }).then((username)=>{
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(()=> console.log("The promise is either resolved or rejected"));
+
+
+
+// 5th promises
+
+// const promiseFive =new Promise(function(resolve,reject){
+//     setTimeout (function(){
+//         let error = true;
+//         if(!error){
+//             resolve({username:'Himanshuk',age:30,email:"himanshu@gamil.com"});
+//         }else{
+//             reject('Error:Js went wrong');
+//         }
+
+//     },1000)
+// })
+
+// async function consumePromiseFive(){
+//     try{
+//         const response=await promiseFive
+//         console.log(response)
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// consumePromiseFive();
