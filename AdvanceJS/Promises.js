532badb07138b9c99f781985a3e17634ebd7e3ad
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2023-09-22 08:44:52 +0000

    promises

--- AdvanceJS/Promises.js
+++ AdvanceJS/Promises.js
@@ -89,3 +89,26 @@ Promisethird.then((user)=>{
 // }
 
 // consumePromiseFive();
+
+//how to api fetch
+
+// async function getAllUsers(){
+//     try{
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json();
+//     console.log(data);
+//    }catch(error){
+//     console.log("E:",error);
+//    }
+// }
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((resopnse)=>{
+    return resopnse.json()
+}).then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.log(error);
+})
